317bdc1440168b4b3c1c4a3924e3b8f49dce7676
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-12-01 23:53:01 -0600

    Optimize part 2

--- 2021/day02/part2.py
+++ 2021/day02/part2.py
@@ -2,18 +2,8 @@ import part1
 
 
 def solution(inp):
-    horizontal = 0
-    depth = 0
-    aim = 0
-    for line in inp:
-        split = line.split(" ")
-        if "forward" in line:
-            horizontal += int(split[1])
-            depth += aim * int(split[1])
-        elif "down" in line:
-            aim += int(split[1])
-        elif "up" in line:
-            aim -= int(split[1])
+    horizontal = part1.sum_term('forward', inp)
+    depth = sum(map(lambda i_v: 0 if 'forward' not in i_v[1] else (part1.sum_term('down', inp[:i_v[0]]) - part1.sum_term('up', inp[:i_v[0]])) * int(i_v[1].split(' ')[1]), enumerate(inp)))
     return horizontal * depth
 
 
