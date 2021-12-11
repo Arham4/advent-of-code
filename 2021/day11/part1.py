d549c2cacb804f2446fc099e1919531336e9697d
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2021-12-10 23:58:01 -0600

    Add initial day 11 part 1 solution

---
+++ 2021/day11/part1.py
@@ -0,0 +1,63 @@
+def in_limits(nums, y, x):
+    if 0 <= y < len(nums) and 0 <= x < len(nums[0]):
+        return True
+    return False
+
+
+flashes = 0
+offsets = [
+    [0, -1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [-1, 1],
+    [-1, 0],
+    [-1, -1],
+]
+
+def print_pretty(nums):
+    for line in nums:
+        for num in line:
+            print(num, end='')
+        print()
+
+
+def try_flash(nums, y, x, visited):
+    global flashes
+    if in_limits(nums, y, x) and (y, x) not in visited:
+        nums[y][x] += 1
+        if nums[y][x] >= 10:
+            visited.add((y, x))
+            nums[y][x] = 0
+            flashes += 1
+            for offset in offsets:
+                try_flash(nums, y + offset[0], x + offset[1], visited)
+
+
+def solution(inp):
+    global flashes
+    nums = []
+    for line in inp:
+        line_list = []
+        for char in line:
+            line_list.append(int(char))
+        nums.append(line_list)
+
+    for i in range(100):
+        visited = set()
+        for y in range(len(inp)):
+            for x in range(len(inp[0])):
+                try_flash(nums, y, x, visited)
+
+    answer = flashes
+    flashes = 0
+    return answer
+
+
+def result(inp):
+    return solution(inp)
+
+
+def test(example_inp):
+    assert result(example_inp) == 1656
---
+++ 2021/day11/part2.py
@@ -0,0 +1,13 @@
+import part1
+
+
+def solution(inp):
+    pass
+
+
+def result(inp):
+    return solution(inp)
+
+
+def test(example_inp):
+    assert result(example_inp) == None
