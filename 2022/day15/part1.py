cd26ebb4d94c47e21c2bc70c584042c0435a4478
Arham4 <arhamjsiddiqui@gmail.com>
2022-12-15 03:48:16 -0600

    Clean up day 15 solution

--- 2022/day15/part1.py
+++ 2022/day15/part1.py
@@ -1,22 +1,50 @@
-import collections
-import sys
+from typing import List
 from collections import *
-import functools
-from functools import *
-import itertools
-from itertools import *
-import math
-from math import *
-import json
-from json import *
-import re
-from re import *
-import heapq
-from heapq import *
 
 
-def manhattan_distance(x1, y1, x2, y2):
+def manhattan_distance(x1: int, y1: int, x2: int, y2: int):
     return abs(x1 - x2) + abs(y1 - y2)
+
+
+def parse_input(inp: List[str]):
+    sensors = set()
+    distances = {}
+    beacons = set()
+    for line in inp:
+        split = line.split(': ')
+        sensor_x = int(split[0][split[0].index('x=') + 2:split[0].index(', ')])
+        sensor_y = int(split[0][split[0].index('y=') + 2:])
+        beacon_x = int(split[1][split[1].index('x=') + 2:split[1].index(', ')])
+        beacon_y = int(split[1][split[1].index('y=') + 2:])
+
+        distance = manhattan_distance(sensor_x, sensor_y, beacon_x, beacon_y)
+        distances[(sensor_x, sensor_y)] = distance
+
+        sensors.add((sensor_x, sensor_y))
+        beacons.add((beacon_x, beacon_y))
+
+    return sensors, distances, beacons
+
+
+def solution(inp: List[str], target: int):
+    locations = defaultdict(set)
+    sensors, distances, beacons = parse_input(inp)
+
+    for sensor in sensors:
+        distance = distances[sensor]
+        distance_to = distance - abs(sensor[1] - target)
+
+        if distance_to <= 0:
+            continue
+
+        locations_at = [sensor[0] + 1 + i for i in range(distance_to) if (sensor[0] + 1 + i, target) not in beacons] \
+                       + [sensor[0] - 1 - i for i in range(distance_to) if (sensor[0] - 1 - i, target) not in beacons] \
+                       + [sensor[0]]
+        locations[target].update(locations_at)
+
+        # fill_manhattan(sensor[0], sensor[1], distance, locations, sensors, beacons)
+
+    return len(locations[target])
 
 
 '''
@@ -36,46 +64,11 @@ def fill_manhattan(x, y, distance, locations, sensors, beacons):
 '''
 
 
-def solution(inp, target):
-    sensors = set()
-    distances = {}
-    beacons = set()
-    locations = defaultdict(set)
-    for line in inp:
-        split = line.split(': ')
-        sensor_x = int(split[0][split[0].index('x=') + 2:split[0].index(', ')])
-        sensor_y = int(split[0][split[0].index('y=') + 2:])
-        beacon_x = int(split[1][split[1].index('x=') + 2:split[1].index(', ')])
-        beacon_y = int(split[1][split[1].index('y=') + 2:])
-
-        distance = manhattan_distance(sensor_x, sensor_y, beacon_x, beacon_y)
-        distances[(sensor_x, sensor_y)] = distance
-
-        sensors.add((sensor_x, sensor_y))
-        beacons.add((beacon_x, beacon_y))
-
-    for sensor in sensors:
-        distance = distances[sensor]
-        distance_to = distance - abs(sensor[1] - target)
-
-        if distance_to <= 0:
-            continue
-
-        locations_at = [sensor[0] + 1 + i for i in range(distance_to) if (sensor[0] + 1 + i, target) not in beacons] \
-                       + [sensor[0] - 1 - i for i in range(distance_to) if (sensor[0] - 1 - i, target) not in beacons] \
-                       + [sensor[0]]
-        locations[target].update(locations_at)
-
-        # fill_manhattan(sensor[0], sensor[1], distance, locations, sensors, beacons)
-
-    return len(locations[target])
-
-
-def result(inp, target):
+def result(inp: List[str], target: int):
     return solution(inp, target)
 
 
-def test(examples):
+def test(examples: List[List[str]]):
     example = 0
     exp = 26
     res = result(examples[example], 10)
